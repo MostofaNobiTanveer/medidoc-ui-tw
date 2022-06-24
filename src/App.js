cd54811ce1f9d30a170a1f50a333bf3e9d1e8de8
unknown <mntanveer9196@gmail.com>
2022-06-24 11:52:47 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Home, Layout } from './components';
+
+const App = () => {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+};
+
+export default App;
---
+++ src/components/theme/RenderThemeChanger.js
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { ThemeContext } from './ThemeContext';
+import { BsSun, BsMoon } from 'react-icons/bs';
+
+const RenderThemeChanger = ({ rounded }) => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  if (theme === 'dark') {
+    return (
+      <button
+        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        className={`${
+          rounded ? 'rounded-full' : 'rounded-lg'
+        } whitespace-nowrap text-lg flex items-center dark:bg-smoky-black border dark:border-hot-pink dark:hover:bg-zinc-900`}
+      >
+        <BsSun className="box-content p-3 text-white" />
+      </button>
+    );
+  } else {
+    return (
+      <button
+        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        className={`${
+          rounded ? 'rounded-full' : 'rounded-lg'
+        } whitespace-nowrap text-lg flex items-center border border-gray-300 bg-gray-50 hover:bg-white dark:hover:bg-zinc-900`}
+      >
+        <BsMoon className="box-content p-3" />
+      </button>
+    );
+  }
+};
+export default RenderThemeChanger;
